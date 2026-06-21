140852d0de2c89fcb0a604e5e78d35836c64d99d
Mel0ny <2189829539@qq.com>
2026-06-22 02:25:11 +0800

    fix(build): 回退导致构建失败的阿里云 Maven 镜像

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,5 @@
 allprojects {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         google()
         mavenCentral()
     }
